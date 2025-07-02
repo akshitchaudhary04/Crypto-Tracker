012817b6466350071a231a41439b08bbfec503f6
Akshit chaudhary <akshit.chaudhary@lenskart.com>
2025-07-02 21:45:36 +0530

    initial setup

---
+++ NetworkLayerProject/App/NetworkLayerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkLayerProjectApp.swift
+//  NetworkLayerProject
+//
+//  Created by Akshit chaudhary on 13/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentListView()
+        }
+    }
+}
---
+++ NetworkLayerProject/Core/Transformer/CoinListTransformer.swift
@@ -0,0 +1,17 @@
+//
+//  CoinListTransformer.swift
+//  NetworkLayerProject
+//
+//  Created by Akshit chaudhary on 14/01/24.
+//
+
+import Foundation
+
+class CoinListTransformer {
+    
+    let decoder: DataDecoder
+    
+    init(decoder: DataDecoder) {
+        self.decoder = decoder
+    }
+}
